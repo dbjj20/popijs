acfca83a5c0fc147de89646366f693d637a744cd
dbjj20 <dbjj20@gmail.com>
2024-06-09 14:48:17 -0400

    update

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
-import { html } from "../main";
 import tinyStore from "../lib/tinyStore";
 
 export function TestComponent({maria}){
   const [getState, addToState] = tinyStore({count: 0})
-  return html`
+  return`
     <button id="TestComponent/addToState" onClick=${() => addToState((prev) => prev.count + 1)}>click me - ${getState().count} - ${maria ? maria : ''}</button>`
 }
